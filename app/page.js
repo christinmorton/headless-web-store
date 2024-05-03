157134d0b0845104f557ba66d869ecd5b1028654
unknown <sysadmin@christinmorton.com>
2024-05-02 23:39:42 -0700

    docker setup incomplete...

--- app/page.js
+++ app/page.js
@@ -3,9 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-xl">Hello World from docker container</h1>
-      <p>We can do more stuff I hope</p>
-      <p>I thought I could add another thing...</p>
+
     </main>
   );
 }
